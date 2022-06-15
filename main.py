f3cafd8e787650fdc3acb29d79df70e7ebab6d71
Patryk Ożóg <s184601@o365.student.pg.edu.pl>
2022-06-16 01:24:18 +0200

    0.5

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ import random
 class Player(object):
 
     def __init__(self):
-        self.player_image = pygame.image.load("wizard1.png").convert_alpha()
+        self.player_image = pygame.image.load("wizard.png").convert_alpha()
         self.treasure_image = pygame.image.load("Treasure.png").convert_alpha()
-        self.player_rect = self.player_image.get_rect(center=(480, 270))
+        self.player_rect = self.player_image.get_rect(center=(960, 540))
         self.rot_image = self.player_image
         self.rot_image_rect = self.player_rect
 
     def move(self, dx, dy):
+        for floor in floors:
+            floor.floor_rect.x -= dx
+            floor.floor_rect.y -= dy
         for enemy in enemies:
             enemy.enemy_rect.x -= dx
             enemy.enemy_rect.y -= dy
@@ -23,6 +26,9 @@ class Player(object):
             wall.wall_rect.x -= dx
             wall.wall_rect.y -= dy
             if self.player_rect.colliderect(wall.wall_rect):
+                for floor in floors:
+                    floor.floor_rect.x += dx
+                    floor.floor_rect.y += dy
                 for enemy in enemies:
                     enemy.enemy_rect.x += dx
                     enemy.enemy_rect.y += dy
@@ -42,10 +48,13 @@ class Player(object):
         starting_point = (self.player_rect.centerx, self.player_rect.centery)
         start_angle = math.radians(self.angle) - math.pi/6
         for ray in range(50):
-            for depth in range(50):
-                target_x = self.player_rect.centerx - math.sin(start_angle) * depth * 5
-                target_y = self.player_rect.centery - math.cos(start_angle) * depth * 5
+            for depth in range(25):
+                target_x = self.player_rect.centerx - math.sin(start_angle) * depth * 30
+                target_y = self.player_rect.centery - math.cos(start_angle) * depth * 30
                 raycast_rect = pygame.Rect(target_x, target_y, 1, 1)
+                for enemy in enemies:
+                    if raycast_rect.colliderect(enemy.enemy_rect):
+                        screen.blit(enemy.enemy_rot_image, enemy.enemy_rot_image_rect)
                 for wall in walls:
                     if raycast_rect.colliderect(wall.wall_rect):
                         #pygame.draw.line(screen, (255, 255, 0), (self.player_rect.centerx, self.player_rect.centery), (target_x, target_y), 4)
@@ -59,7 +68,7 @@ class Player(object):
             start_angle += math.pi / 150
 
         for point in points:
-            pygame.gfxdraw.filled_polygon(screen, (point, starting_point, self.player_rect.center), (255, 255, 100, 70))
+            pygame.gfxdraw.filled_polygon(screen, (point, starting_point, self.player_rect.center), (255, 255, 100, 4))
             starting_point = point
             #pygame.draw.circle(screen, (0, 255, 255), point, 5)
             #screen.blit(enemy.enemy_rot_image, enemy.enemy_rot_image_rect)
@@ -72,43 +81,128 @@ class Player(object):
 class Wall(object):
 
     def __init__(self, pos):
-        self.wall_image = pygame.image.load("floor1.png").convert_alpha()
+        self.wall_image = pygame.image.load("Wall.png").convert_alpha()
         self.wall_image.set_alpha(50)
         walls.append(self)
-        self.wall_rect = pygame.Rect(pos[0], pos[1], 40, 40)
+        self.wall_rect = pygame.Rect(pos[0], pos[1], 128, 128)
+
+class Floor(object):
+
+    def __init__(self, pos):
+        self.floor_image = pygame.image.load("Floor.png").convert_alpha()
+        self.floor_image.set_alpha(50)
+        floors.append(self)
+        self.floor_rect = pygame.Rect(pos[0], pos[1], 128, 128)
 
 
 class Enemy(object):
 
     def __init__(self, pos):
         enemies.append(self)
-        self.enemy_rect = pygame.Rect(pos[0], pos[1], 32, 32)
-        self.enemy_image = pygame.image.load("Spider.png").convert_alpha()
+        self.enemy_rect = pygame.Rect(pos[0], pos[1], 95, 95)
+        self.enemy_image = pygame.image.load("Monster.png").convert_alpha()
         self.enemy_rot_image = self.enemy_image
         self.enemy_rot_image_rect = self.enemy_rect
+        self.player_visable = False
+        self.reset_function = True
+        self.point_of_interest = (random.randrange(0, 1920), random.randrange(0, 1080))
+        self.temporary_point = self.point_of_interest
+        self.direction = random.randrange(0, 4)
+        self.timer = 5
 
     def move_towards_player(self):
-        dx, dy = player.player_rect.x - self.enemy_rect.x, player.player_rect.y - self.enemy_rect.y
-        dist = math.hypot(dx, dy)
+        if self.player_visable:
+            self.timer = 0
+        dirvect = pygame.math.Vector2(player.player_rect.x - self.enemy_rect.x,  player.player_rect.y - self.enemy_rect.y)
         try:
-            dx, dy = dx / dist, dy / dist
-        except ZeroDivisionError:
-            dx, dy = dx, dy
-        self.enemy_rect.x += dx * 3
-        self.enemy_rect.y += dy * 3
+            dirvect.normalize()
+        except ValueError:
+            dirvect.x += 0.01
+            dirvect.y += 0.01
+        dirvect.scale_to_length(10)
+        self.enemy_rect.move_ip(dirvect)
+        for wall in walls:
+            if self.enemy_rect.colliderect(wall.wall_rect):
+                self.enemy_rect.move_ip(-dirvect)
+                if wall.wall_rect.bottom == self.enemy_rect.top or wall.wall_rect.top == self.enemy_rect.bottom:
+                    self.enemy_rect.x += dirvect.x
+                if wall.wall_rect.left == self.enemy_rect.right or wall.wall_rect.right == self.enemy_rect.left:
+                    self.enemy_rect.y += dirvect.y
+
+    def move_random_direction(self):
+        if self.reset_function:
+            self.direction = random.randrange(0, 4)
+        self.reset_function = False
+        if self.direction == 0:
+            self.enemy_rect.x += 15
+        elif self.direction == 1:
+            self.enemy_rect.x -= 15
+        elif self.direction == 2:
+            self.enemy_rect.y += 15
+        elif self.direction == 3:
+            self.enemy_rect.y -= 15
+        for wall in walls:
+            if self.enemy_rect.colliderect(wall.wall_rect):
+                if self.direction == 0:
+                    self.enemy_rect.x -= 15
+                elif self.direction == 1:
+                    self.enemy_rect.x += 15
+                elif self.direction == 2:
+                    self.enemy_rect.y -= 15
+                elif self.direction == 3:
+                    self.enemy_rect.y += 15
+                self.reset_function = True
 
     def enemy_rotate(self):
-        dx, dy = player.player_rect.centerx - self.enemy_rect.centerx, player.player_rect.centery - self.enemy_rect.centery
+        if self.direction == 0:
+            dx, dy = 10000, self.enemy_rect.centery
+        elif self.direction == 1:
+            dx, dy = -10000, -self.enemy_rect.centery
+        elif self.direction == 2:
+            dx, dy = self.enemy_rect.centerx, 10000
+        elif self.direction == 3:
+            dx, dy = -self.enemy_rect.centerx, -10000
+        if self.player_visable or self.timer < 5:
+            dx, dy = player.player_rect.centerx - self.enemy_rect.centerx, player.player_rect.centery - self.enemy_rect.centery
         self.angle = math.degrees(math.atan2(-dy, dx)) + 90
         self.enemy_rot_image = pygame.transform.rotate(self.enemy_image, self.angle)
         self.enemy_rot_image_rect = self.enemy_rot_image.get_rect(center=self.enemy_rect.center)
 
+    def enemy_cast_rays(self):
+        start_angle = math.radians(self.angle) - math.pi / 6
+        lock = False
+        for ray in range(20):
+            for depth in range(12):
+                target_x = self.enemy_rect.centerx + math.sin(start_angle) * depth * 70
+                target_y = self.enemy_rect.centery + math.cos(start_angle) * depth * 70
+                raycast_rect = pygame.Rect(target_x, target_y, 1, 1)
+                if raycast_rect.colliderect(player.player_rect):
+                    self.player_visable = True
+                    lock = True
+                elif not lock:
+                    self.player_visable = False
+                for wall in walls:
+                    if raycast_rect.colliderect(wall.wall_rect):
+                        #pygame.draw.line(screen, (255, 255, 0), (self.enemy_rect.centerx, self.enemy_rect.centery), (target_x, target_y), 4)
+                        break
+                    #else:
+                        #pygame.draw.line(screen, (255, 255, 0), (self.enemy_rect.centerx, self.enemy_rect.centery), (target_x, target_y), 4)
+                else:
+                    continue
+                break
+            start_angle += math.pi / 60
+
     def enemy_update(self):
-        for enemy in enemies:
+        enemy.enemy_rotate()
+        enemy.enemy_cast_rays()
+        if self.player_visable:
             enemy.move_towards_player()
-            enemy.enemy_rotate()
-
-
+        else:
+            self.timer += 0.05
+            if self.timer < 5:
+                enemy.move_towards_player()
+            else:
+                enemy.move_random_direction()
 
 # os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
@@ -118,61 +212,59 @@ screen = pygame.display.set_mode((1920, 1080))
 
 clock = pygame.time.Clock()
 walls = []
+floors = []
 enemies = []
 player = Player()
 
 level = (
-    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W        WW                  W"
-    "W        WW                  W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W                            W"
-    "W          T                 W"
-    "W                            W"
-    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+    "WWWWWWWWWWWWWWWWWWWWWWWWW"
+    "W                       W"
+    "W                       W"
+    "W        WW             W"
+    "W        WW             W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W                       W"
+    "W          T            W"
+    "W                       W"
+    "WWWWWWWWWWWWWWWWWWWWWWWWW"
 )
 
 x = y = 0
-for rows in range(30):
-    for columns in range(30):
-        square = rows*30+columns
+for rows in range(25):
+    for columns in range(25):
+        square = rows*25+columns
         if level[square] == "W":
             Wall((x, y))
+        if level[square] == " ":
+            Floor((x, y))
         if level[square] == "T":
             end_rect = pygame.Rect(x, y, 20, 20)
-        x += 40
-    y += 40
+        x += 128
+    y += 128
     x = 0
 
-enemies_generated = random.randrange(1, 4)
+enemies_generated = 2 #random.randrange(1, 4)
 for i in range(enemies_generated):
     enemy_x, enemy_y = random.randrange(100, 1000), random.randrange(100, 1000)
     Enemy((enemy_x, enemy_y))
 
 
-icon = pygame.image.load("agent.png")
+icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 
 
@@ -189,27 +281,30 @@ while running:
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a]:
-        player.move(-4, 0)
+        player.move(-18, 0)
     if key[pygame.K_d]:
-        player.move(4, 0)
+        player.move(18, 0)
     if key[pygame.K_w]:
-        player.move(0, -4)
+        player.move(0, -18)
     if key[pygame.K_s]:
-        player.move(0, 4)
+        player.move(0, 18)
 
     if player.player_rect.colliderect(end_rect):
         pygame.quit()
         sys.exit()
 
-    screen.fill((255, 255, 255))
+    screen.fill((25, 25, 25))
+    for floor in floors:
+        screen.blit(floor.floor_image, floor.floor_rect)
+    screen.blit(player.treasure_image, end_rect)
+    player.player_update()
+    screen.blit(player.rot_image, player.rot_image_rect)
+    #screen.blit(player.floor_image, (100, 100))
     for wall in walls:
         screen.blit(wall.wall_image, wall.wall_rect)
-    screen.blit(player.treasure_image, end_rect)
-    screen.blit(player.rot_image, player.rot_image_rect)
-    player.player_update()
     for enemy in enemies:
-        screen.blit(enemy.enemy_rot_image, enemy.enemy_rot_image_rect)
         enemy.enemy_update()
+        #screen.blit(enemy.enemy_rot_image, enemy.enemy_rot_image_rect)
     pygame.display.flip()  # flip/update?
     clock.tick(360)
 
